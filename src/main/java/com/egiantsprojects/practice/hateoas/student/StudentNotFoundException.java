33215dec1fdd6753e4459c9ced0b8d6e10c0b051
rajasekharchallari009 <challariraj@gmail.com>
2019-07-21 04:11:42 -0400

    EgiantsTasks: Successfully Integrated REST - HATEOAS

---
+++ src/main/java/com/egiantsprojects/practice/hateoas/student/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package com.egiantsprojects.practice.hateoas.student;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into student
+values(10001,'Raj', 'm123569');
+
+insert into student
+values(10002,'Ramsay', 'o258963');
